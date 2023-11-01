0d59cb153954b4f9df881642aff5a97e7f872189
Reshna Raghu Chalil <84814192+rchalil@users.noreply.github.com>
2023-11-01 11:21:41 -0400

    Add files via upload

---
+++ Libs/TempTestSuite1698847576726.groovy
@@ -0,0 +1,36 @@
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.exception.StepFailedException
+import com.kms.katalon.core.reporting.ReportUtil
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.testdata.TestDataColumn
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import internal.GlobalVariable as GlobalVariable
+
+Map<String, String> suiteProperties = new HashMap<String, String>();
+
+suiteProperties.put('id', 'Test Suites/Reshna - All TCs Suite2')
+suiteProperties.put('name', 'Reshna - All TCs Suite2')
+suiteProperties.put('description', '')
+suiteProperties.put('rerunTestFailImmediately', 'true')
+suiteProperties.put('retryCount', '3')
+ 
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner())
+
+
+
+RunConfiguration.setExecutionSettingFile("C:\\Users\\rchalil\\Katalon Studio\\My First Web UI Project\\Reports\\20231101_100616\\Reshna - All TCs Suite2\\20231101_100616\\execution.properties")
+
+TestCaseMain.beforeStart()
+
+TestCaseMain.startTestSuite('Test Suites/Reshna - All TCs Suite2', suiteProperties, new File("C:\\Users\\rchalil\\Katalon Studio\\My First Web UI Project\\Reports\\20231101_100616\\Reshna - All TCs Suite2\\20231101_100616\\testCaseBinding"))
